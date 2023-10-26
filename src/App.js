2e819209b8c31b2745cd2d68491fa301c84cf99b
amir <mashhadifarahani1@gmail.com>
2023-10-26 16:06:15 +0330

    adding the header

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+        <Navbar/>
+      <div className='shoe__header'>
+      </div>
     </div>
   );
 }
