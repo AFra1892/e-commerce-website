9bc3cb70ed9d9311c6c40f1c4223a8f6876ebea5
amir <mashhadifarahani1@gmail.com>
2023-10-31 22:48:26 +0330

    new menu using antd library

--- src/App.js
+++ src/App.js
@@ -4,10 +4,56 @@ import './App.css';
 import Popular from './components/popular/Popular';
 import Footer from './components/footer/Footer';
 
+import {Menu} from 'antd'
+
+<>
+    <p><a href='/'>Home</a></p>
+    <p><a href='/menu'>Menu</a></p>
+    <p><a href='/about'>About</a></p>
+    <p><a href='#gallery'>Gallery</a></p>
+    <p><a href='#contact'>Contact</a></p>
+    <i class='bx bx-shopping-bag' ></i>
+  </>
+
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Menu
+      mode='horizontal'
+        items={
+          [
+            {
+              label:"Home",
+              key:"Home",
+              
+            },
+            {
+              label:"Men",
+              key:"Men",
+              children:[
+                {
+                  label:"Shoe",
+                  key:"Shoe"
+                },
+                {
+                  label:"T-shirt",
+                  key:"T-shirt"
+                }
+              ]
+            },
+            {
+              label:"Women",
+              key:"Women"
+            },
+            {
+              label:"About",
+              key:"About"
+            }
+          ]
+        }
+      >
+
+      </Menu>
       <div className='shoe__header'></div>
       <Trend/>
       <Popular/>
