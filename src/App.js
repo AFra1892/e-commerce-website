ecea441e4022f7461b88d11c63da3bf117ae1de1
amir <mashhadifarahani1@gmail.com>
2023-11-02 15:31:43 +0330

    changing navbar component

--- src/App.js
+++ src/App.js
@@ -1,53 +1,14 @@
-import Navbar from './components/header/Navbar';
 import Trend from './components/main/Trend';
 import './App.css';
 import Popular from './components/popular/Popular';
 import Footer from './components/footer/Footer';
-import {Menu} from 'antd'
-import { StyleProvider } from '@ant-design/cssinjs';
+import NewNav from './components/header/NewNav';
 
 
 function App() {
   return (
     <div className="App">
-      <StyleProvider hashPriority="high">
-      <Menu
-      className='test-menu'
-      mode='horizontal'
-        items={
-          [
-            {
-              label:"Home",
-              key:"Home",
-              
-            },
-            {
-              label:"Men",
-              key:"Men",
-              children:[
-                {
-                  label:"Shoe",
-                  key:"Shoe"
-                },
-                {
-                  label:"T-shirt",
-                  key:"T-shirt"
-                }
-              ]
-            },
-            {
-              label:"Women",
-              key:"Women"
-            },
-            {
-              label:"About",
-              key:"About"
-            }
-          ]
-        }
-      >
-      </Menu>
-      </StyleProvider>
+      <NewNav/>
       <div className='shoe__header'></div>
       <Trend/>
       <Popular/>
---
+++ src/components/header/NewNav.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import './newNavbar.css'
+const NewNav = () => {
+  return (
+    
+<div class="menu-container">
+<nav>
+  <ul class="menu">
+    <li class="dropdown dropdown-9">
+        New & Featured
+        <ul class="dropdown_menu dropdown_menu--animated dropdown_menu-9">
+            <li class="dropdown_item-1">Item 1</li>
+            <li class="dropdown_item-2">Item 2</li>
+            <li class="dropdown_item-3">Item 3</li>
+            <li class="dropdown_item-4">Item 4</li>
+            <li class="dropdown_item-5">Item 5</li>
+        </ul>
+    </li>
+  </ul>
+</nav>
+</div>
+
+  )
+}
+
+export default NewNav
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // <li class="dropdown dropdown-1">
+    //   New & Featured
+    // </li>
+    //  <li class="dropdown dropdown-2">
+    //   Men
+    //   <ul class="dropdown_menu dropdown_menu-5">
+    //     <li class="dropdown_item-1">Item 1</li>
+    //     <li class="dropdown_item-2">Item 2</li>
+    //     <li class="dropdown_item-3">Item 3</li>
+    //       <li class="dropdown_item-4">Item 4</li>
+    //       <li class="dropdown_item-5">Item 5</li>
+    //   </ul>
+    // </li>
+    // <li class="dropdown dropdown-3">
+    //   Women
+    // </li>
+    // <li class="dropdown dropdown-4">
+    //   Accessories
+    // </li>
+    // <li class="dropdown dropdown-5">
+    //   Sales
+      
+    // </li>
